f9b303e80b6899bc7304cd3788de1c22eca729a0
RyanmjDev <123763401+RyanmjDev@users.noreply.github.com>
2023-09-20 19:38:19 -0500

    Can now preview on mobile

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Used so we can preview on mobile, delete later
+  server: {
+    host: '0.0.0.0',
+    https: false 
+  }
 })
